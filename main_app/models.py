f8f8ae2dc876d2b3ce038b32e40ba08b93a6161f
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-01-21 12:25:59 -0800

    Complete Step 2

---
+++ main_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Widget(models.Model):
+    widget = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.widget 
+
---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Widget
+
+# Create your views here.
+
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widget': widgets})
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = "__all__"
+    success_url = "/"
